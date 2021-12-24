a5de330b126b35467ea69b14e8644143f4d0c59e
nepp-plus <nepp.plus@gmail.com>
2021-12-24 10:00:43 +0900

    책 정보 출력 기능 (메쏘드) 추가
    - self 변수를 이용해, 어떤 책인지 확인 가능.
    - self.하위변수를 이용해, 책의 하위 정보들 확인 가능.
    - 다른 기능에서 self.변수 = 값 형태로 self에 붙인 변수만 가져와서 사용 가능

--- book.py
+++ book.py
@@ -17,3 +17,10 @@ class Book:
         self.t = title  # 책 한권의 t 변수에 => title에 적힌 값 기록.
         self.rf = rent_fee
         self.la = limit_age
+        
+    # 책의 정보를 출력하는 기능.
+    def print_book_info(self):  # 파라미터 필요 없다.
+        print('===== 도서 정보 출력 =====')
+        print(f'제목 : {self.t}')  # 명령을 시킨 책의 제목에 접근
+        print(f'대여료 : {self.rf}')
+        print(f'연령제한 : {self.la}세 이용가')
