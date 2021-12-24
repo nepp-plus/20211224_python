079c4af6fd79d3612743ab820c75f4a92a7cfd7e
nepp-plus <nepp.plus@gmail.com>
2021-12-24 10:57:57 +0900

    메쏘드와의 차이점 점검용 함수 추가

--- main.py
+++ main.py
@@ -26,3 +26,11 @@ book2.print_book_info()
 book3 = Book()
 book3.set_data('뽀로로', 500, 0)
 book3.print_book_info()
+
+
+# 함수와의 비교를 위한 예시 코드.
+def add_and_print(num1, num2):
+    print(num1 + num2)
+    
+
+add_and_print(5, 11)
